7943372aa4f9ac0aad9e4ff1d9893e55c24b2934
Rad14nt <andrei@gimli.ro>
2026-01-13 10:37:26 +0100

    CICD pipeline adjust base path for CD

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     output: "standalone",
-    basePath: "/seb-documentation",
-    assetPrefix: "/seb-documentation",
+    basePath: "/seb-documentation"
 };
 
 export default nextConfig;
