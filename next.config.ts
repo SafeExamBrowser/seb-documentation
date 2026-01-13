3f885cc9ff2791b58572ac1003d1f22e10db37b6
Rad14nt <andrei@gimli.ro>
2026-01-13 10:52:12 +0100

    CICD pipeline

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     output: "standalone",
-    basePath: "/seb-documentation"
+    basePath: "/seb-documentation",
+    assetPrefix: "/seb-documentation",
 };
 
 export default nextConfig;
