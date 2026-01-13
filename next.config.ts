e7cfd7b870794190b18f40b6ab973590f54512b7
Rad14nt <andrei@gimli.ro>
2026-01-13 13:42:28 +0100

    Remove base path completely

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     output: "standalone",
-    basePath: "/seb-documentation",
-    assetPrefix: "/seb-documentation",
 };
 
 export default nextConfig;
